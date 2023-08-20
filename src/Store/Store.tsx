2f1ff6c278fc722e7b80c7eaef93906102eef6dc
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-08-20 09:24:43 -0300

    Reorder structure project

--- src/Store/Store.tsx
+++ src/Store/Store.tsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice";
+import userReducer from "./user/UserSlice";
 
 export const store = configureStore ({
     reducer: {
